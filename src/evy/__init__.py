475bdebeb28a94ed14bd20d2fc86bd9eb6aabc78
Farhan Reynaldo <farhanreynaldo@gmail.com>
2025-09-12 11:35:19 +0700

    Load land cover and mask by cropland

--- src/evy/__init__.py
+++ src/evy/__init__.py
@@ -7,10 +7,12 @@ from .core import (
     plot_phenology,
     zonal_stats,
 )
-from .loader import fetch_boundaries, open_evi
+from .loader import open_evi, load_land_cover, fetch_boundaries
+from .processing import mask_evi_by_cropland
 
 __all__ = [
     "open_evi",
+    "load_land_cover",
     "fetch_boundaries",
     "aggregate_by",
     "compute_phenology",
@@ -19,4 +21,5 @@ __all__ = [
     "compute_aggregated_anomalies",
     "compute_difference",
     "plot_phenology",
+    "mask_evi_by_cropland",
 ]
